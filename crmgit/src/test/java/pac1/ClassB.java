cbab6a5475e2de8aaf2d7e6ef568c01649a87c74
DELL <DELL@Rashmi>
2024-12-20 12:19:52 +0530

    Done the changes as mentoned

--- crmgit/src/test/java/pac1/ClassB.java
+++ crmgit/src/test/java/pac1/ClassB.java
@@ -2,7 +2,8 @@ package pac1;
 
 public class ClassB {
 	public static void main(String[] args) {
-		
+	System.out.println("hiiiiiiiiiiiiiiiiiiii");
+	System.out.println("ddjfdskfjhkdsjhf");
 	}
 
 }
